47690c49582481f3bc5ab08208338ec2816c5bc3
Daniel Lousa <dlousa92@gmail.com>
2018-07-03 14:54:18 -0400

    have herocard access database and list all heroes

--- src/Components/HeroCard/Hero-Card.js
+++ src/Components/HeroCard/Hero-Card.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class HeroCard extends Component {
+  constructor () {
+    super()
+    this.state = {
+      heroes: []
+    }
+  }
+
+  componentDidMount () {
+    axios.get('http://localhost:8000/api/heroes')
+      .then((res) => {
+        console.log(res)
+        this.setState({
+          heroes: res.data
+        })
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
   render () {
+    const heroes = this.state.heroes.map((hero, i) => {
+      return (
+        <div key={i}>
+          <p>{hero.name}</p>
+          <p>{hero.superPower}</p>
+        </div>
+      )
+    })
+
     return (
       <div>
-        <h1>Sup</h1>
+        { heroes }
       </div>
     )
   }
