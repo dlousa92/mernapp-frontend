1978a243e7ad0dd84dbaa421ab2d2def5d1bb042
Daniel Lousa <dlousa92@gmail.com>
2018-07-03 17:03:49 -0400

    start to add functionality for delete

--- src/Components/HeroCard/Hero-Card.js
+++ src/Components/HeroCard/Hero-Card.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import { Link, Route } from 'react-router-dom'
 
 class HeroCard extends Component {
   constructor () {
@@ -10,9 +11,10 @@ class HeroCard extends Component {
   }
 
   componentDidMount () {
+    console.log('here')
     axios.get('http://localhost:8000/api/heroes')
       .then((res) => {
-        console.log(res)
+        // console.log(res)
         this.setState({
           heroes: res.data
         })
@@ -21,12 +23,21 @@ class HeroCard extends Component {
         console.log(err)
       })
   }
+
   render () {
+    console.log(this.state)
     const heroes = this.state.heroes.map((hero, i) => {
       return (
         <div key={i}>
-          <p>{hero.name}</p>
+          <p><Link to={`/heroes/${hero._id}`}>{hero.name}</Link></p>
           <p>{hero.superPower}</p>
+          <Route path={`/heroes/${hero._id}`}
+            render={() => {
+              return (
+                <input type='submit' value='Submit' />
+              )
+            }}
+          />
         </div>
       )
     })
