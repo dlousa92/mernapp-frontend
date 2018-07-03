195842540c74a80309221e5e6f1d2a0504e0922b
Daniel Lousa <dlousa92@gmail.com>
2018-07-03 16:10:21 -0400

    create form in which a player can add a hero to the battle

--- src/Components/New/New.js
+++ src/Components/New/New.js
@@ -1,12 +1,54 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class New extends Component {
+  constructor () {
+    super()
+
+    this.state = {
+      name: '',
+      superPower: ''
+    }
+    this.createHero = this.createHero.bind(this)
+    this.setName = this.setName.bind(this)
+    this.setSuperPower = this.setSuperPower.bind(this)
+    this.onClick = this.onClick.bind(this)
+  }
   render () {
     return (
       <div>
-        <h1>welcome</h1>
+        <h1>Add a new hero to the battle below</h1>
+        <form onSubmit={this.onClick}>
+          <input type='text' placeholder='Name of Hero' onChange={this.setName} />
+          <input type='text' placeholder='Superpowers?' onChange={this.setSuperPower} />
+          <input type='submit' value='Submit' />
+        </form>
       </div>
     )
+  }
+  createHero (e) {
+    e.preventDefault()
+
+    axios.post('http://localhost:8000/api/heroes', {
+      name: this.state.name,
+      superPower: this.state.superPower
+    })
+      .then(function (res) {
+      })
+  }
+  onClick (e) {
+    this.createHero(e)
+    this.props.history.push('/')
+  }
+  setName (e) {
+    this.setState({
+      name: e.target.value
+    })
+  }
+  setSuperPower (e) {
+    this.setState({
+      superPower: e.target.value
+    })
   }
 }
 
